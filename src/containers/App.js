60541113fcbb1b9744aa3c1e9b63cf22b4f86922
adityaitalia <43336753+adityaitalia@users.noreply.github.com>
2020-07-08 22:53:09 +0530

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import CardList from '../components/CardList';
+import {robots} from '../robots';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll'
+
+class App extends Component{
+	constructor(){
+		super();
+		this.state={
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) =>{
+		this.setState({searchfield: event.target.value});
+		
+	}
+	render(){
+		const { robots, searchfield}= this.state;
+		const filteredrobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		});
+		return (
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredrobots}/>
+				</Scroll>
+			</div>
+		);
+	}
+	
+}
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Aditya Italia',
+    username: 'Italia',
+    email: 'italiaaditya@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Harsh Vora',
+    username: 'Antonette',
+    email: 'hagdu@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Saharsh Barve',
+    username: 'Samantha',
+    email: 'b3@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Siddharth Shah',
+    username: 'Karianne',
+    email: 'overthinker@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Vedant RC',
+    username: 'Kamren',
+    email: 'vrc@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolf',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clement DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'padberg@karina.biz'
+  }
+];
